edad1d85242021d72fd9f0c42af7d1477c55ef5b
Tsiky <tsiky@test.com>
2022-07-28 14:22:39 +0300

    Create component option generator and send value to question form

---
+++ src/components/QuestionGenerator/AnswerGenerator.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+function AnswerGeneretor(props) {
+  const [formFields, setFormFields] = useState([{ name: "" }, { name: "" }]);
+
+  const handleFormChange = (event, index) => {
+    let data = [...formFields];
+    data[index][event.target.name] = event.target.value;
+    setFormFields(data);
+  };
+
+  const addField = () => {
+    let object = {
+      name: "",
+    };
+
+    setFormFields([...formFields, object]);
+  };
+
+  const submit = (e) => {
+
+    e.preventDefault();
+    props.setOption(formFields)
+
+    setFormFields([{ name: "" }, { name: "" }]);
+  };
+
+  const removeFields = (index) => {
+    let data = [...formFields];
+    data.splice(index, 1);
+    setFormFields(data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        {formFields.map((form, index) => {
+          return (
+            <div key={index}>
+              <input
+                name="name"
+                placeholder="Option"
+                onChange={(event) => handleFormChange(event, index)}
+                value={form.name}
+              />
+
+              <button
+                className="btn btn-default"
+                style={{ margin: "10px" }}
+                onClick={() => removeFields(index)}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })}
+      </form>
+      <div style={{ margin: "10px" }}>
+        <button className="btn btn-primary" onClick={addField}>
+          Add option
+        </button>
+        <button className="btn btn-primary" onClick={submit}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AnswerGeneretor;
