25a00ad8d175bad381494110581efbcce9b51b72
Tsiky <tsiky@test.com>
2022-07-28 14:20:57 +0300

    add component of main question generator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import AppBarTodo from './components/appBar/AppBar'
+
+import QuestionMain from './components/QuestionGenerator/QuestionMain'
 function App() {
   return (
     <div className="App">
       <AppBarTodo/>
+      <QuestionMain/>
     </div>
   );
 }
