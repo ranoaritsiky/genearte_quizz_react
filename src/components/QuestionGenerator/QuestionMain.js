68a7d723ba3bb8b243690aca20c39f99220e1da1
Tsiky <tsiky@test.com>
2022-07-28 14:26:06 +0300

    add question form tio questionmain

---
+++ src/components/QuestionGenerator/QuestionMain.js
@@ -0,0 +1,11 @@
+import QuestionForm from './QuestionForm'
+
+const QuestionMain =()=>{
+    return (
+        <div className="d-flex flex-row p-3" style={{ height: "100vh" }}>
+            <QuestionForm/>
+        </div>
+    )
+}
+
+export default QuestionMain
