29739da4c85602f366ea55f3666d94f228c830c0
Tsiky <tsiky@test.com>
2022-07-28 11:43:31 +0300

    initialize actions and initial state for questions

---
+++ src/store/actions.js
@@ -0,0 +1,13 @@
+export const questionFormInitialState = [];
+
+export const ADD_QUESTION = "add question";
+export const DELETE_QUESTION = "delete question";
+
+export const add_question = (question) => {
+  return {
+    type: ADD_QUESTION,
+    question,
+  };
+};
+
+
