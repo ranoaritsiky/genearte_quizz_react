67c2ff44254f0e68146eb9545d4326f04b09ef5c
Tsiky <tsiky@test.com>
2022-07-28 17:41:49 +0300

    show checkbox

--- src/components/QuestionGenerator/QuestionList.js
+++ src/components/QuestionGenerator/QuestionList.js
@@ -2,20 +2,25 @@ import { useSelector } from "react-redux";
 
 const QuestionList = () => {
   const questionState = useSelector((state) => state);
-//   console.log(questionState)
+  //   console.log(questionState)
   return (
     <div className="d-flex">
       {questionState.map((q, i) => (
-        <div key={i} className="card" >
+        <div key={i} className="card">
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
-                <span>Q- </span> {q.question}
+              <span>Q- </span> {q.question}
             </li>
             <li className="list-group-item">
-                <div>{q.code}</div>
+              <div>{q.code}</div>
             </li>
             <li className="list-group-item">
-                {console.log(q.option)}
+              {q.option.map((op, index) => (
+                <div key={index}>
+                  <input type="checkbox"/>
+                  {op.name}
+                </div>
+              ))}
             </li>
           </ul>
         </div>
